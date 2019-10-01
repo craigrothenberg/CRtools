50348d15df3ed5356b8af278bfa152823a1aece8
craigroth <craigroth@users.noreply.github.com>
2019-10-01 10:01:09 -0400

    added library(scales) to projectBuilder
    both for _import.R and _libs.R

--- R/CRtools.R
+++ R/CRtools.R
@@ -37,11 +37,11 @@ ProjectBuilder <- function(){
 
   file.create("code\\_import.R")
   fileConn.import <- file("code\\_import.R")
-  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)"),fileConn.import)
+  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)","library(scales)"),fileConn.import)
 
   file.create("code\\_libs.R")
   fileConn.import <- file("code\\_libs.R")
-  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)"),fileConn.import)
+  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)","library(scales)"),fileConn.import)
 
   file.create("code\\_documentation.R")
   fileConn.documentation <- file("code\\_documentation.R")
