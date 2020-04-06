890bd9f883080f49e8d09d8b4c9dfb8b1955a5cd
punchdeck <62629105+punchdeck@users.noreply.github.com>
2020-04-06 09:30:42 -0400

    Update CRtools.R
    created pRound to convert p values to character and anything <0.0001 is reported as '<0.0001'

--- R/CRtools.R
+++ R/CRtools.R
@@ -10,6 +10,18 @@ round2 <- function( vec , digits=0 ){
   vec.round <- sign(vec0) * vec / 10^digits
   return(vec.round)
 }
+# 2020-04-06 rounds p values below 0.0001 to '<0.0001'; converts to character
+pRound <- function( vec , digits=4 ){
+  vec0 <- vec
+  eps <- 10^(-10)
+  vec <- abs(vec)
+  vec <- vec*10^digits
+  vec2 <- vec-floor(vec)
+  #    vec <- floor( vec ) + ifelse( vec2 < .5 , 0 , 1 )
+  vec <- floor( vec ) + ifelse( ( vec2 - .5 ) < - eps, 0 , 1 )
+  vec.round <- sign(vec0) * vec / 10^digits
+  if(vec.round<0.0001){return('<0.0001')} else{return(as.character(vec.round))}
+}
 
 # convert number to percent as character variable
 # input:
