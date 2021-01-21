3cbdcae188cbf0acced7268056d84552ea01a9bb
punchdeck <62629105+punchdeck@users.noreply.github.com>
2021-01-21 13:13:43 -0500

    fixed error in CRProjectBuilder for writing text

--- R/CRtools.R
+++ R/CRtools.R
@@ -49,19 +49,23 @@ CRprojectBuilder <- function(){
 
   file.create("code\\_import.R")
   fileConn.import <- file("code\\_import.R")
-  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)","library(scales)","library(skimr)","library(car)","library(ggfortify)","library(lubridate)",fileConn.import))
+  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)","library(scales)","library(skimr)","library(car)","library(ggfortify)","library(lubridate)"),fileConn.import)
+  close(fileConn.import)
 
   file.create("code\\_libs.R")
   fileConn.libs <- file("code\\_libs.R")
-  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)","library(scales)","library(skimr)","library(car)","library(ggfortify)","library(lubridate)",fileConn.libs))
+  writeLines(c("library(tidyverse)","library(CRtools)","library(cowplot)","library(ggthemes)","library(readxl)","library(scales)","library(skimr)","library(car)","library(ggfortify)","library(lubridate)"),fileConn.libs)
+  close(fileConn.libs)
 
   file.create("code\\_documentation.R")
   fileConn.documentation <- file("code\\_documentation.R")
-  writeLines(c("dat.doc <- vector('list')","dat.doc[['Notes']] <- 'Enter Notes Here'",fileConn.documentation))
+  writeLines(c("dat.doc <- vector('list')","dat.doc[['Notes']] <- 'Enter Notes Here'"),fileConn.documentation)
+  close(fileConn.documentation)
 
   file.create("code\\_analysis.R")
   file.create("code\\_functions.R")
   file.create("code\\_diagnostics.R")
+  file.create("code\\_old code.R")
 
   file.edit("code\\_import.R")
   file.edit("code\\_libs.R")
